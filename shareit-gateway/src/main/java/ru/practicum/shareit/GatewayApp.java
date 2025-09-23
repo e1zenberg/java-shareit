1ea390eb1e22bb3875a731fdc750def03c36aa78
e1zenberg <e1zenbergdev@gmail.com>
2025-09-23 11:03:48 +0300

    v 1.6 правки pom

--- shareit-gateway/src/main/java/ru/practicum/shareit/GatewayApp.java
+++ shareit-gateway/src/main/java/ru/practicum/shareit/GatewayApp.java
@@ -1,9 +1,13 @@
 package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class GatewayApp {
 
     public static void main(String[] args) {
